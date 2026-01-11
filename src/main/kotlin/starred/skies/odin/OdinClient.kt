e46c8c3d43759843db672efbab33f01eab53170f
skies-starred <skies.starred@gmail.com>
2026-01-11 20:32:37 +0530

    internal(UpdateNotifier)

--- src/main/kotlin/starred/skies/odin/OdinClient.kt
+++ src/main/kotlin/starred/skies/odin/OdinClient.kt
@@ -1,6 +1,7 @@
 package starred.skies.odin
 
 import com.odtheking.odin.config.ModuleConfig
+import com.odtheking.odin.events.core.EventBus
 import com.odtheking.odin.features.Module
 import com.odtheking.odin.features.ModuleManager
 import com.odtheking.odin.utils.getCenteredText
@@ -12,6 +13,7 @@ import net.minecraft.network.chat.ClickEvent
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.HoverEvent
 import net.minecraft.network.chat.Style
+import starred.skies.odin.features.UpdateNotifier
 import starred.skies.odin.features.impl.cheats.*
 import starred.skies.odin.helpers.Scribble
 import java.net.URI
@@ -22,15 +24,17 @@ object OdinClient : ClientModInitializer {
         Highlight, AutoClicker, Gloomlock, EscrowFix, AutoGFS, QueueTerms, AutoTerms, SimonSaysAdditions
     )
 
-    private const val MOD_VERSION: String = /*$ mod_version*/ "0.1.2"
     private val moduleConfig: ModuleConfig = ModuleConfig("odinClient")
     private val mainFile: Scribble = Scribble("main")
 
     private var lastInstall: String by mainFile.string("lastInstall")
     private var send: Boolean = true
 
+    const val MOD_VERSION: String = /*$ mod_version*/ "0.1.2"
+
     override fun onInitializeClient() {
         ModuleManager.registerModules(moduleConfig, *modulesToRegister)
+        EventBus.subscribe(UpdateNotifier)
 
         ClientPlayConnectionEvents.JOIN.register { _, _, _ ->
             if (!send) return@register
@@ -49,7 +53,7 @@ object OdinClient : ClientModInitializer {
         modMessage("Thank you for installing OdinClient §8(v$MOD_VERSION)§f.", "")
         modMessage("", "")
         modMessage("Quick start:", "")
-        modMessage("  §b/odin §7- Open configuration menu")
+        modMessage("  §b/odin §7- Open configuration menu", "")
         modMessage("", "")
 
         val message = Component.literal("Need help or want to suggest features? Click to join the Discord!")
